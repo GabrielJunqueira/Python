14e74bcc9668cdbb20a3dc65b12ac3b7694177f0
Gabriel Junqueira <64487574+GabrielJunqueira@users.noreply.github.com>
2022-05-23 08:09:19 -0300

    Create medias.py

---
+++ medias.py
@@ -0,0 +1,8 @@
+first = int(input("Digite a primeira nota:"))
+second = int(input("Digite a segunda nota:"))
+third = int(input("Digite a terceira nota:"))
+fourth = int(input("Digite a quarta nota:"))
+
+media = (first + second + third + fourth)/4
+
+print("A média aritmética é",media)
